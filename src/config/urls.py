2d1dcf69d1778ab6b905c5ad3dea9fd9b6ee53b0
Azat Seidakhmet <seidakhmetazat@gmail.com>
2023-09-08 22:55:59 +0600

    certs

--- src/config/urls.py
+++ src/config/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path("i18n/", include("django.conf.urls.i18n")),
+    path("i18n/", include("django.conf.urls.i18n")),
     path("api/admin/", admin.site.urls),
 ]
 

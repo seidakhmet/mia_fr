a5a834372f6a918f172cf17928dde180954a04c5
Azat Seidakhmet <seidakhmetazat@gmail.com>
2023-09-08 22:25:41 +0600

    certs

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("/api/admin", admin.site.urls),
+    # path("i18n/", include("django.conf.urls.i18n")),
+    path("api/admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:

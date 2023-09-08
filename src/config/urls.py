3568e8c29375ee6b5c5203241a4787bcb4fb39e4
Azat Seidakhmet <seidakhmetazat@gmail.com>
2023-09-08 22:23:08 +0600

    certs

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,11 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import RedirectView
+from django.urls import path
 
 urlpatterns = [
-    path("/", RedirectView.as_view(url="/api/admin", permanent=False)),
     path("/api/admin", admin.site.urls),
 ]
 
